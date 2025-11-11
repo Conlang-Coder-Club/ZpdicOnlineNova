5e315046f346f80afbd7351e760573cb5f2dc588
yamada_taro_06 <yamada.taro.06.40@gmail.com>
2025-11-12 03:08:42 +0900

    [add] art-x-sm.ymlを追加
    yml書くの初めて()
    ちな"art-x-sm"の"sm"は"symbol"の略で、全部記号と絵文字で表す言語。

--- client/message/index.ts
+++ client/message/index.ts
@@ -7,6 +7,7 @@ export const messageInventory = {
   ["ja"]: () => import("./ja.yml"),
   ["en"]: () => import("./en.yml"),
   ["eo"]: () => import("./eo.yml"),
+  ["art-x-sm"]: () => import("./art-x-sm.yml"),
   ["isv-Cyrl"]: () => import("./isv.yml"),
   ["isv-Latn"]: () => import("./isv.yml").then((messages) => convertMessages(messages.default, convertInterslavicCyrillicToLatin))
 } as any as MessageInventory;
